fc32f0ab2dced599b1d8ff26e5582d8fc9bb7868
Aleksei Beloshytski <master.whitish@gmail.com>
2014-04-23 21:37:03 +0300

    Update mapper.R

--- mapper.R
+++ mapper.R
@@ -3,4 +3,5 @@ f<-file("stdin")
 g<-readLines(f)
 
 for (i in unlist(strsplit(g," ")))
-  cat(i,"\t",nchar(i),"\n")
+  #cat(i,"\t",nchar(i),"\n") #map <word> \t <length>
+  cat(substr(i,0,1),"\t",nchar(i),"\n") #map <first letter in word> \t <word's length>
